f4bc5402c8f8adf6ffe3d6fd94556d01a94ef6a5
vasilii <porokh.vs@gmail.com>
2025-06-30 11:14:38 +0300

    STYLE

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -4,8 +4,8 @@ export enum STYLE_KEYS {
 }
 
 const STYLES = {
-    [STYLE_KEYS.RED]: 'color: #ffaaaaaa; background: #000000',
-    [STYLE_KEYS.YELLOW]:'color: #000000; background: #FFFF00',  
+    [STYLE_KEYS.RED]: STYLE_KEYS.RED,
+    [STYLE_KEYS.YELLOW]: STYLE_KEYS.YELLOW,  
 }
 
 export const log = (msg: string, styleKey: STYLE_KEYS | null = null, ...args: any[]) => {
